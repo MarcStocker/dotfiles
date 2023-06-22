6072054f2ea020485701d4d77224db02b9596da9
Marc <marc@Marcs-MacBook-Pro.local>
2023-06-22 10:54:27 -0700

    minor change

--- scripts/sshToServers.sh
+++ scripts/sshToServers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 script_name1=`basename $0`
-script_path1=$(dirname $(readlink -f $0))
+script_path1=$(dirname "$(readlink $0)")
 script_path_with_name="$script_path1/$script_name1"
 server_path_with_name="$script_path1/.serversToSshToo.sh"
 
